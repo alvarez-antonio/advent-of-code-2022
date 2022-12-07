39455b9b2abcbfbc06b1f22f0a9ceef3236b2f83
António Alvarez <totalvarez@gmail.com>
2022-12-07 10:30:35 +0000

    :fire: Day 5 part 2 complete

--- app/Day05.hs
+++ app/Day05.hs
@@ -29,34 +29,52 @@ destruct move = do
   let pieces = splitOn " " move
   (read (pieces !! 1), read (pieces !! 3), read (pieces !! 5))
 
-doInstructions :: Seq String -> Int -> Int -> Int -> Seq String
-doInstructions stacks 0 source destination = stacks
-doInstructions stacks howMany source destination = do
+doInstructionOneAtATime :: Seq String -> Int -> Int -> Int -> Seq String
+doInstructionOneAtATime stacks 0 source destination = stacks
+doInstructionOneAtATime stacks howMany source destination = do
   let updatedDestination = (stacks `index` (destination - 1)) ++ [last (stacks `index` (source - 1))]
       updatedSource = init (stacks `index` (source - 1))
       firstUpdate = update (destination - 1) updatedDestination stacks
       updatedStacks = update (source - 1) updatedSource firstUpdate
-  trace ("Stack updated " ++ show updatedStacks) doInstructions updatedStacks (howMany - 1) source destination
+  trace ("Stack updated " ++ show updatedStacks) doInstructionOneAtATime updatedStacks (howMany - 1) source destination
 
-applyInstruction :: Seq String -> (Int, Int, Int) -> Seq String
-applyInstruction stack instruction = do
+doInstructionInBulk :: Seq String -> Int -> Int -> Int -> Seq String
+doInstructionInBulk stacks howMany source destination = do
+  let sourceStack = stacks `index` (source - 1)
+      destinationStack = stacks `index` (destination - 1)
+      updatedDestination = destinationStack ++ drop (length sourceStack - howMany) sourceStack
+      updatedSource = take (length sourceStack - howMany) sourceStack
+      firstUpdate = update (destination - 1) updatedDestination stacks
+      updatedStacks = update (source - 1) updatedSource firstUpdate
+  trace ("Stack updated " ++ show updatedStacks) updatedStacks
+
+applyInstruction :: (Seq String -> Int -> Int -> Int -> Seq String) -> Seq String -> (Int, Int, Int) -> Seq String
+applyInstruction doInstructionFunction stack instruction = do
   let (howMany, source, destination) = instruction
-  trace ("Move to be processed " ++ show instruction) doInstructions stack howMany source destination
+  trace ("Move to be processed " ++ show instruction) doInstructionFunction stack howMany source destination
 
-process :: Seq String -> [(Int, Int, Int)] -> Seq String
-process stacks [] = stacks
-process stacks instructions = do
-  let newStacks = applyInstruction stacks (head instructions)
-  process newStacks (tail instructions)
+process :: (Seq String -> Int -> Int -> Int -> Seq String) -> Seq String -> [(Int, Int, Int)] -> Seq String
+process doInstructionFunction stacks [] = stacks
+process doInstructionFunction stacks instructions = do
+  let newStacks = applyInstruction doInstructionFunction stacks (head instructions)
+  process doInstructionFunction newStacks (tail instructions)
 
 part1 :: [String] -> Int
 part1 fileLines = do
   let firstSplit = splitOn [""] fileLines
       stacks = fromList $ parseStacks $ head firstSplit
       moves = trace ("Stacks " ++ show stacks) map destruct $ last firstSplit
-      result = trace ("Moves " ++ show moves) process stacks moves
+      result = trace ("Moves " ++ show moves) process doInstructionOneAtATime stacks moves
       list = trace ("Result " ++ show result) toList result
       sentence = map last list
   trace ("sentence " ++ show sentence) 1
 
-part2 fileLines = 1
+part2 :: [String] -> Int
+part2 fileLines = do
+  let firstSplit = splitOn [""] fileLines
+      stacks = fromList $ parseStacks $ head firstSplit
+      moves = trace ("Stacks " ++ show stacks) map destruct $ last firstSplit
+      result = trace ("Moves " ++ show moves) process doInstructionInBulk stacks moves
+      list = trace ("Result " ++ show result) toList result
+      sentence = map last list
+  trace ("sentence " ++ show sentence) 1
